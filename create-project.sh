92c29a0218a758822f15c72dc2eee6122ed79c40
Daniel Holmen <daniel.holmen@mac-c02p50wpg3qd.local>
2016-08-31 09:40:35 +0200

    Added workaround for when TMPDIR isn't defined

--- create-project.sh
+++ create-project.sh
@@ -2,9 +2,13 @@
 
 CHECKOUTDIR=$TMPDIR/bv-xp-bootstrapper
 
+if [ -z "$TMPDIR" ]; then
+  echo "No \$TMPDIR variable defined. Cloning into current directory instead."
+  CHECKOUTDIR=./bv-xp-bootstrapper
+fi
+
 if [ -d "$CHECKOUTDIR" ]; then
   rm -rf $CHECKOUTDIR
-  echo "fins"
 fi
 
 mkdir $CHECKOUTDIR
@@ -14,4 +18,3 @@ git clone https://git.bouvet.no/scm/exm/enonic-xp-vanilla-starter.git $CHECKOUTD
 source $CHECKOUTDIR/scripts/setup/create-project.sh
 
 rm -rf $CHECKOUTDIR
-
